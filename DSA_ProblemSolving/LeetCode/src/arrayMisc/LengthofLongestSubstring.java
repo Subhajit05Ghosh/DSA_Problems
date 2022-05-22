830e46c02f8e6d7d1d9c5c6b1a2bf9681aba962e
KT718UJ <speak.subhajitghosh@gmail.com>
2022-05-23 02:15:07 +0530

    Updates

---
+++ DSA_ProblemSolving/LeetCode/src/arrayMisc/LengthofLongestSubstring.java
@@ -0,0 +1,30 @@
+package arrayMisc;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class LengthofLongestSubstring {
+    public static void main(String[] args) {
+        String str = "takeUforward";
+        System.out.println("The length of the longest substring without repeating characters is " + solve(str));
+    }
+
+    private static int solve(String str) {
+        int a_pointer=0;
+        int b_pointer=0;
+        int max=0;
+        LinkedHashSet<Character> hs=new LinkedHashSet<>();
+        while(b_pointer<str.length()){
+        if(!hs.contains(str.charAt(b_pointer))){
+            hs.add(str.charAt(b_pointer));
+            b_pointer++;
+            max=Math.max(hs.size(),max);
+        }
+        else{
+            hs.remove(str.charAt(a_pointer));
+            a_pointer++;
+        }
+        }
+        System.out.println(hs.toString());
+    return max;}
+}
