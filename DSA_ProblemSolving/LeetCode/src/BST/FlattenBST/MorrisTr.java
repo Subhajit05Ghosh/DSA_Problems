2ad1a8c475e8be2ed667f5f7863d92355ede3beb
KT718UJ <speak.subhajitghosh@gmail.com>
2022-08-12 01:17:54 +0530

    Updates

--- DSA_ProblemSolving/LeetCode/src/BST/FlattenBST/MorrisTr.java
+++ DSA_ProblemSolving/LeetCode/src/BST/FlattenBST/MorrisTr.java
@@ -33,21 +33,21 @@ public class MorrisTr {
 
     public static void main(String args[]) {
 
-/*        Node root = new Node(1);
+        Node root = new Node(1);
         root.left = new Node(2);
         root.left.left = new Node(3);
         root.left.right = new Node(4);
         root.right = new Node(5);
         root.right.right = new Node(6);
-        root.right.right.left = new Node(7);*/
-        Node root = new Node(5);
+        root.right.right.left = new Node(7);
+        /*Node root = new Node(5);
         root.left = new Node(3);
         root.right = new Node(7);
         root.left.left = new Node(2);
         root.left.right = new Node(4);
         root.right.left = new Node(6);
         root.right.right = new Node(8);
-
+*/
         flatten(root);
         while (root.right != null) {
             System.out.print(root.data + "->");
